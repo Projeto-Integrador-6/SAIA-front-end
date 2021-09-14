9e15de8e766a2f0514a80b5fb02b2727966c2678
Luiz <zluizfs.outlook.com>
2021-09-14 20:59:48 -0300

    Rotas básicas

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom';
 
 import { ThemeProvider } from '@material-ui/styles';
 
-import Home from './pages/Home';
-import Login from './pages/Login';
+import Routes from './routes';
 
 import UiTheme from './theme';
 
 import './global.css';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={UiTheme}>
-      <Login />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
