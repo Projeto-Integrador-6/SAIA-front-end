d528662ca2743bfa56ec7eccb81015bb7fa36487
Luiz <zluizfs.outlook.com>
2021-09-15 20:12:26 -0300

    Integrando home com sidebar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,29 +1,32 @@
 import React from 'react';
 
 import Box from '../../components/Box'
+import Sidebar from '../../components/Sidebar';
 
 import './index.css'
 
-export default function Home(){
-  return(
-    <div className="home-container">
-      <h1>Home</h1>
-      <div className="side-navigation">
-        <h3>Navegação</h3>
-        <div className="boxes-container">
-          <Box title="AVALIAÇÕES" link="/avaliacoes"/>
-          <Box title="ALUNOS" link="/alunos"/>
-          <Box title="TESTE" link="/teste"/>
+export default function Home() {
+  return (
+    <Sidebar>
+      <div className="home-container">
+        <h1>Home</h1>
+        <div className="side-navigation">
+          <h3>Navegação</h3>
+          <div className="boxes-container">
+            <Box title="AVALIAÇÕES" link="/avaliacoes" />
+            <Box title="ALUNOS" link="/alunos" />
+            <Box title="TESTE" link="/teste" />
+          </div>
+        </div>
+        <div className="side-resume">
+          <h3>Resumo</h3>
+          <div className="boxes-container">
+            <Box title="AVALIAÇÕES EM ANDAMENTO" content={10} />
+            <Box title="AVALIAÇÕES CRIADAS" content={12} />
+          </div>
         </div>
       </div>
-      <div className="side-resume">
-        <h3>Resumo</h3>
-        <div className="boxes-container">
-          <Box title="AVALIAÇÕES EM ANDAMENTO" content={10}/>
-          <Box title="AVALIAÇÕES CRIADAS" content={12}/>
-        </div>
-      </div>
-    </div>
+    </Sidebar>
   )
 }
 
